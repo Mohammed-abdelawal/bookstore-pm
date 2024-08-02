1950d26b9e3e43d5d4520682ecb84425e07fc4c1
mabdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-08-02 22:29:12 +0300

    add admin for Book, Reviews

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from .models import Book, Review
 
-# Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publish_date", "created_at")
+    list_filter = ("author", "publish_date")
+    search_fields = ("title", "author", "description")
+    date_hierarchy = "publish_date"
+    inlines = [ReviewInline]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("book", "user", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("book__title", "user__username", "review_text")
+    date_hierarchy = "created_at"
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin)
