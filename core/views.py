5f9ce28d272fc3085c66d0758714d94b177920ab
mabdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-08-02 22:58:44 +0300

    add annotations to views

--- core/views.py
+++ core/views.py
@@ -5,23 +5,43 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, BookSerializer, ReviewSerializer
 from .models import Book
+from drf_yasg.utils import swagger_auto_schema
 
 UserModel = get_user_model()
 
 
 class RegisterView(generics.CreateAPIView):
+    """
+    Register a new user.
+    """
+
     queryset = UserModel.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
 
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Retrieve a list of books or a specific book.
+    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Add a review to a book.",
+        request_body=ReviewSerializer,
+        responses={
+            status.HTTP_201_CREATED: ReviewSerializer,
+            status.HTTP_400_BAD_REQUEST: "Validation Error",
+        },
+    )
     @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
     def add_review(self, request, pk=None):
+        """
+        Add a review to a specific book.
+        """
         book = self.get_object()
         data = request.data
         data["book"] = book.id
